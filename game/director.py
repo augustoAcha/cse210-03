d767b0b303301a2010965910416c46118dda8278
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-01-21 13:26:57 -0300

    "updated code"

--- game/director.py
+++ game/director.py
@@ -1,4 +1,31 @@
+from game.cards import Cards
+class Director:
+    
+    def __init__(self):
+        self.cards=[]
+        self.isPlaying=True
 
+        for i in range(13):
+            cards = Cards()
+            self.cards.append(cards)
+        print("dentro del init de la Clase Director")
+           ### #print(f"Espacio en memoria  para la {i}° carta  ",self.cards[i])
+    
+    def startgame(self):
+        while self.isPlaying:
+            card = Cards()
+            print("------------------------------")
+            print()
+            print()
+            print("The card is : ", card.player_choice())
+
+            p = input("Do you Play  y / n ?")          
+            if p.lower() != "y":
+                self.isPlaying = False
+                print(" Game is Over ")
+                print("Thank you and cameback soon!") 
+
+'''
 from game.cards import Cards
 class Director:
     
@@ -19,6 +46,7 @@ class Director:
             print()
             print()
             #print("The card is : ", card.player_choice())
+            
 
             p = input("Do you Play  y / n ?")          
             if p.lower() != "y":
@@ -26,3 +54,4 @@ class Director:
                 print(" Game is Over ")
                 print("Thank you and cameback soon!")    
 
+'''

ff04ca6fe9aa497787e7794770749a69865b22da
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-01-21 19:38:23 -0300

    Update director.py
    "checking code packages work correctly"

--- game/director.py
+++ game/director.py
@@ -8,9 +8,7 @@ class Director:
         for i in range(13):
             cards = Cards()
             self.cards.append(cards)
-        print("dentro del init de la Clase Director")
-           ### #print(f"Espacio en memoria  para la {i}° carta  ",self.cards[i])
-    
+       
     def startgame(self):
         while self.isPlaying:
             card = Cards()
@@ -18,12 +16,12 @@ class Director:
             print()
             print()
             print("The card is : ", card.player_choice())
-
+            
             p = input("Do you Play  y / n ?")          
             if p.lower() != "y":
                 self.isPlaying = False
                 print(" Game is Over ")
-                print("Thank you and cameback soon!") 
+                print("Thank you and cameback soon!")    
 
 '''
 from game.cards import Cards
